346dc04c5d5f2c6f19adbcd9ad238918618fb9a9
Ferenczi Benjámin <66739561+Ptolemajosz@users.noreply.github.com>
2024-04-23 16:23:47 +0200

    Update ora10.cpp

--- ora10.cpp
+++ ora10.cpp
@@ -13,8 +13,22 @@ int db(int n) {
     return db;
 }
 
+int max_fv(int tomb[10]){
+    int max = tomb[0];
+    int idx = 0;
+    for(int i = 0; i < 10; i++) {
+        if(max <= tomb[i]) {
+            max = tomb[i];
+            idx = i;
+        }
+    }
+    return idx;
+}
+
 int main() {
 
-    cout << db(20);
+    cout << db(20) << endl;
+    int tomb[10] = {1,2,3,4,5,6,7,8,9,10};
+    cout << max_fv(tomb) << endl;
     return 0;
 }
