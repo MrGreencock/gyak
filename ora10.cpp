a38315e5ba7b23fd9a1493447dddd68bceb53ee9
Ferenczi Benjámin <66739561+Ptolemajosz@users.noreply.github.com>
2024-04-23 16:33:37 +0200

    Update ora10.cpp

--- ora10.cpp
+++ ora10.cpp
@@ -25,10 +25,30 @@ int max_fv(int tomb[10]){
     return idx;
 }
 
+int gyak(int tomb[10]) {
+    int max = 0;
+    int num = 0;
+
+    for(int i = 0; i < 10; i++) {
+        int db = 0;
+        for(int j = 0; j < 10; j++) {
+            if(tomb[i] == tomb[j]) {
+                db++;
+            }
+        }
+        if(db > max) {
+            max = db;
+            num = tomb[i];
+        }
+    }
+    return num;
+}
+
 int main() {
 
     cout << db(20) << endl;
-    int tomb[10] = {1,2,3,4,5,6,7,8,9,10};
-    cout << max_fv(tomb) << endl;
+    int tomb[10] = {1,1,2,3,3,3,4,5,5,6};
+    cout << gyak(tomb) << endl;
+
     return 0;
 }
