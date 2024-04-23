fbb501878405e026b06d830804c669f6756a4570
Ferenczi Benjámin <66739561+Ptolemajosz@users.noreply.github.com>
2024-04-23 16:19:05 +0200

    Update ora10.cpp

--- ora10.cpp
+++ ora10.cpp
@@ -2,8 +2,19 @@
 #include <vector>
 
 using namespace std;
+//n-nel is működik
+int db(int n) {
+    int db = 0;
+    for(int i = 1; i <= n/2; i++) {
+        if(n % i == 0) {
+            db++;
+        }
+    }
+    return db;
+}
 
 int main() {
-    
+
+    cout << db(20);
     return 0;
 }
